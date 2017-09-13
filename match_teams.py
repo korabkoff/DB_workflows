c6a51535358c07ca9ea5ce5f99d3c7f99adfa458
korabkoff <korabkoff@gmail.com>
2017-09-13 20:44:58 +0300

    working prototype

--- match_teams.py
+++ match_teams.py
@@ -3,7 +3,6 @@ import numpy as np
 import time
 
 
-
 # get all directories in current one
 
 def get_list_of_dirs(root_folder_path):
@@ -32,9 +31,6 @@ def load_data(file_path):
         return content
 
 
-
-
-
 # get team plaers IDs by team ID
 def get_team_ids(teams_players_id, team_id):
 
@@ -52,7 +48,7 @@ def get_team_score(team_id):
 
 
 # get sorted list of teams by score
-def get_sorted_list_of_teams_score(teams_filepath, players_scores):
+def get_sorted_list_of_teams_by_score(teams_filepath, players_scores):
 
     teams_id_n_score = dict()
 
@@ -78,16 +74,19 @@ def get_sorted_list_of_teams_score(teams_filepath, players_scores):
                 teams_id_n_score[int(team_id)] = team_score
 
             # print('teams_score: '+ str(team_score))
-
-    return sorted(teams_id_n_score.items(), key=lambda team_score: team_score[1])
+        # print (teams_id_n_score)
+    return sorted(teams_id_n_score.items(), key=lambda score: score[1])
 
 
 # return teams pairwise starting from first
-    def return_pairwise(sorted_by_score):
-        for index in len(sorted_by_score):
-            if index%2:
+def return_pairwise_id(sorted_by_score):
 
-        return sorted_by_score[0][0],sorted_by_score[1][0]
+    sorted_by_score = [id for id, score in (sorted_by_score)]
+
+    matched_teams = list(zip(sorted_by_score[0:len(sorted_by_score):2],
+                         sorted_by_score[1:len(sorted_by_score):2]))
+
+    return matched_teams
 
 
 # make dict of Players scores to efficient serch in it
@@ -114,30 +113,29 @@ def make_players_scores_dict(players_filepath):
     return players_scores
     # return player_id
 
+
+
+
+
 if __name__ == '__main__':
     start_time = time.time()
-    # list_of_dirs = get_list_of_dirs(os.getcwd())
-    # print('list_of_files: ', list_of_dirs)
-    # for directory in list_of_dirs:
-    #     players = load_data(directory+'players.txt')
-    #     teams_players_id = load_data(directory+'teams.txt')
+
     directory = 'test_A'
 
-    # players_score = load_data(directory +'/'+'players.txt')
-    # print('players_score: ' + str(players_score[:10]))
-
-    # teams_players_id = load_data(directory +'/'+'teams.txt')
-
-    # print('teams_players_id: '+ str(teams_players_id[:2]))
-    #
-    # player_score = get_player_score(players_score, 9)
-    # print('player_score: ' + player_score)
-    #
-    # team_ids = get_team_ids(teams_players_id, 1)
-    # print('team_ids: ' + str(team_ids))
     players_scores_dict = make_players_scores_dict(directory +'/'+'players.txt')
-
+    print(time.time() - start_time)
     # print('players_scores: ' + str(players_scores_dict[:10]))
-    sorted_by_score = get_sorted_list_of_teams_score(directory +'/'+'teams.txt', players_scores_dict)
+
+    sorted_by_score = get_sorted_list_of_teams_by_score(directory +'/'+'teams.txt', players_scores_dict)
     print(sorted_by_score[:10])
+    print(time.time() - start_time)
+
+    matched = return_pairwise_id(sorted_by_score)
+    print(matched[:5])
+    print(time.time() - start_time)
+    filepath = directory +'/'+'pairs.txt'
+    with open(filepath, 'w') as output:
+        for team_a, team_b in matched:
+            output.write(str(team_a) + ' ' + str(team_b) + '\n')
+
     print(time.time() - start_time)
