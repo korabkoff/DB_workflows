3c3186071c2921537ce156c9660330e963be42f1
korabkoff <korabkoff@gmail.com>
2017-09-14 21:34:40 +0300

    cleanup from debugging and time checking improve output to correspond requirements  time for 4 tasks 5.1 sec

--- match_teams.py
+++ match_teams.py
@@ -1,22 +1,16 @@
 import os
-import numpy as np
-import time
+
+# run from directory with tests
 
 
-# get all directories in current one
+def get_sub_directories():
 
-def get_list_of_dirs(root_folder_path):
+    sub_dirs = []
+    for root, dirs, files in os.walk(os.getcwd()):
+        sub_dirs.extend(dirs)
 
-    if not root_folder_path:
-        return None
+    return sub_dirs
 
-    list_of_dir_name = []
-
-    # get all files in subdirectories
-    for root, dirs, files in os.walk(root_folder_path):
-        list_of_dir_name.extend(dirs)
-
-    return list_of_dir_name
 
 def load_players_scores(file_path):
     if not file_path or not os.path.exists(file_path):
@@ -35,38 +29,44 @@ def load_teams_players(file_path):
 
 
 def match_teams(all_teams_players, all_players_scores):
+    if not all_teams_players or not all_players_scores:
+        return None
 
     teams_id_n_score = dict()
 
     for team_id, team_players in enumerate(all_teams_players):
 
-        team_score = int()
+        # team_score = int()
 
         teams_id_n_score[team_id] = sum([int(all_players_scores[int(player)])
-                                                    for player in team_players])
+                                        for player in team_players])
 
     sorted_teams = [id for id, score in (sorted(teams_id_n_score.items(), key=lambda score: score[1]))]
 
     return list(zip(sorted_teams[0:len(sorted_teams):2],
-                             sorted_teams[1:len(sorted_teams):2]))
+                    sorted_teams[1:len(sorted_teams):2]))
+
+
+def output_matched_teams(pairs_filepath, matched_teams):
+    if not matched_teams:
+        return None
+
+    with open(pairs_filepath, 'w') as output:
+        for team_a, team_b in matched_teams:
+            output.write(str(team_a) + ' ' + str(team_b) + '\n')
 
 
 if __name__ == '__main__':
-    start_time = time.time()
 
-    directory = 'test_A'
-    players_filepath = directory +'/'+'players.txt'
-    teams_filepath = directory +'/'+'teams.txt'
+    for directory in get_sub_directories():
+        players_filepath = directory + '/' + 'players.txt'
+        teams_filepath = directory + '/' + 'teams.txt'
 
-    pairs = match_teams(load_teams_players(teams_filepath), load_players_scores(players_filepath))
+        out_dir = 'Korabkoff_task_1_team_pairs'
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        pairs_filepath = out_dir + '/' + directory + '_pairs.txt'
 
-    # players_scores_dict = make_players_scores_dict(directory +'/'+'players.txt')
-    print(time.time() - start_time)
-    print('pairs: ' + str(pairs[:10]))
+        output_matched_teams(pairs_filepath, match_teams(load_teams_players(teams_filepath),
+                                                         load_players_scores(players_filepath)))
 
-    filepath = directory +'/'+'pairs.txt'
-    with open(filepath, 'w') as output:
-        for team_a, team_b in pairs:
-            output.write(str(team_a) + ' ' + str(team_b) + '\n')
-
-    print(time.time() - start_time)
